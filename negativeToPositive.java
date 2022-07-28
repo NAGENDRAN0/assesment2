059d65a30e9cf1d8fe38f60fc5b7198409d249cb
NAGENDRAN0 <109665849+NAGENDRAN0@users.noreply.github.com>
2022-07-28 22:57:56 +0530

    Add files via upload

---
+++ negativeToPositive.java
@@ -0,0 +1,16 @@
+package week2.day2;
+
+public class negativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=-60;
+		if(a<0) {
+			System.out.println(a);
+			int a1=a*(-1);
+			System.out.println(a1);
+		}
+
+	}
+
+}
---
+++ positiveornegative.java
@@ -0,0 +1,18 @@
+package week2.day2;
+
+public class positiveornegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		if(a>0) {
+			System.out.println("positive");}
+	   else {
+			
+				System.out.println("negative");
+		}
+			
+
+	}
+
+}
